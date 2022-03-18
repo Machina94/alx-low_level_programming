c3d0708b8584acd312cfc8fbeeb8702dc739e127
Machina94 <chinedumaduka94@yahoo.com>
2022-03-18 06:20:54 -0700

    helloo put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
