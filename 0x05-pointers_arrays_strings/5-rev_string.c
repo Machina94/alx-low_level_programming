84567b8024f048d29a8ec6cfaf042c3ddd8bafeb
Machina94 <chinedumaduka94@yahoo.com>
2022-03-21 21:29:09 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string character
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+		x++;
+	while (s[x])
+	{
+		x--;
+	}
+}
