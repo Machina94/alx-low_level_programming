3bd542d0e4b3bee026a891da1d9e2b496fb2257b
Machina94 <chinedumaduka94@yahoo.com>
2022-03-24 19:13:04 -0700

    new dirs

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 int _putchar(char c);
 
